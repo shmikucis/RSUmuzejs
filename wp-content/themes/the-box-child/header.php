be61ab0134d55702b5181775e58a2bc9ac29cf5a
Vilnis Laipnieks <vl12030@lu.lv>
2015-08-05 11:03:14 +0300

    the-box child theme View first post to see template. Copy style.css from style_backup to use parent theme.

---
+++ wp-content/themes/the-box-child/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @since The Box 1.0
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/parallax.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">	
+
+        
+
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+	
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<div id="page">
+
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header clearfix" role="banner">
+		<div class="top_bar">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="RSU muzejs" />
+			</a>
+		</div>
+</header>
+		
+		
