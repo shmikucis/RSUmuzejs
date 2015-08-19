beb7448b7bc492f1b02d97875798916008f5aa23
Guntis <guntis.smaukstelis@gmail.com>
2015-08-19 14:19:53 +0300

    wp-config.php update for easier copy to production

--- wp-config.php
+++ wp-config.php
@@ -16,22 +16,21 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'RSUmuzejs');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', '');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+if("http://$_SERVER[HTTP_HOST]" == 'http://localhost'){
+	define('DB_NAME', 'RSUmuzejs');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'root');
+	define('DB_HOST', 'localhost');
+	define('DB_CHARSET', 'utf8');
+	define('DB_COLLATE', '');	
+} else {
+	define('DB_NAME', 'rsunanol_rsu'); 
+	define('DB_USER', 'rsunanol_rsu');
+	define('DB_PASSWORD', 'We-6*-F!Anvo');
+	define('DB_HOST', 'localhost');
+	define('DB_CHARSET', 'utf8');
+	define('DB_COLLATE', '');
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
